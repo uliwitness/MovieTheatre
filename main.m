79748d78a9b82fbc04f4ed295cabba298620bb3e
Uli Kusterer <ulivc@zathras.de>
2010-11-19 22:33:04 +0100

    Remove UKCustomWindowFrame use from MovieTheatre.

--- main.m
+++ main.m
@@ -11,21 +11,13 @@
 // -----------------------------------------------------------------------------
 
 #import <Cocoa/Cocoa.h>
-#import "UKCustomWindowFrame.h"
 
 
 // -----------------------------------------------------------------------------
 //  main:
-//      Main entry point. Take care of installing our window pattern override
-//      before any other applications get instantiated.
 // -----------------------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
-    NSAutoreleasePool*  pool = [[NSAutoreleasePool alloc] init];
-    [UKCustomWindowFrame installCustomWindowFrame];
-    [UKCustomWindowFrame setCustomWindowColor: [NSColor colorWithPatternImage: [NSImage imageNamed: @"tv_background"]]];
-    [pool release];
-    
-    return NSApplicationMain(argc,  (const char **) argv);
+	return NSApplicationMain(argc,  (const char **) argv);
 }
